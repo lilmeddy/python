374872c8a76b0699af35760ec22d3ae478bff743
lilmeddy <aghedopriscilla@gmail.com>
2024-03-06 09:08:38 +0100

    string Manipulation

--- stringMani.py
+++ stringMani.py
@@ -1,7 +1,9 @@
 firstName = "osamede"
 lastName = "aghedo"
+accountBalance = 10
+accountNumber = 2134567890
 # print(firstName.capitalize())
 # print(firstName.upper())
 # print(firstName.lower())
 
-print(f"Welcome back {firstName} {lastName}")
+print(f"Hi {firstName.capitalize()} {lastName.capitalize()}. Your Account balance is ${accountBalance} and your Account Number is {accountNumber}")
