1fd87a8085ff917fe42d12057fa0ff089e82f49f
lilmeddy <aghedopriscilla@gmail.com>
2024-03-06 10:07:09 +0100

    string Manipulation going on

--- stringMani.py
+++ stringMani.py
@@ -1,12 +1,25 @@
-firstName = input("What is your first name ")
-lastName = input("What is your last name ")
-accountBalance = input("what is your account balance ")
-accountNumber = input("what is your account number ")
+# firstName = input("What is your first name ")
+# lastName = input("What is your last name ")
+# accountBalance = input("what is your account balance ")
+# accountNumber = input("what is your account number ")
 # print(firstName.capitalize())
 # print(firstName.upper())
 # print(firstName.lower())
 
 # print(f"Hi {firstName.capitalize()} {lastName.capitalize()}.\nYour Account balance is ${accountBalance}\nYour Account Number is {accountNumber}")
-print(f'''Hi {firstName.capitalize()} {lastName.capitalize()}.
-Your Account balance is ${accountBalance}
-Your Account Number is {accountNumber}''')
+# print(f'''Hi {firstName.capitalize()} {lastName.capitalize()}.
+# Your Account balance is ${accountBalance}
+# Your Account Number is {accountNumber}''')
+
+# Assignment ask student about their scores in science, math and english exam
+# Ask for their name, print admission number and find the mean score and write your average score is
+
+firstName = input("What is your first name ")
+lastName = input("What is your last name ")
+math = int(input(f'Dear {firstName} {lastName} enter your score in maths exam '))
+science = int(input(f'Dear {firstName} {lastName} enter your score in science exam '))
+english = int(input(f'Dear {firstName} {lastName} enter your score in english exam '))
+add = math + science + english
+mean = add/3
+print() 
+
