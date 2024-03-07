32e5e472a232912864032a067934afaeeeb8c31d
lilmeddy <aghedopriscilla@gmail.com>
2024-03-07 09:15:12 +0100

    string Manipulation going on

--- stringMani.py
+++ stringMani.py
@@ -14,16 +14,16 @@
 # Assignment ask student about their scores in science, math and english exam
 # Ask for their name, print admission number and find the mean score and write your average score is
 
-firstName = input("What is your first name ")
-lastName = input("What is your last name ")
-math = int(input(f'Dear {firstName} {lastName} enter your score in maths exam '))
-science = int(input(f'Dear {firstName} {lastName} enter your score in science exam '))
-english = int(input(f'Dear {firstName} {lastName} enter your score in english exam '))
-add = math + science + english
-mean = add/3
-print(f'''Dear {firstName.capitalize()} {lastName.capitalize()}
-Congratulation you have been offered admission into our University 
-With an average score of {mean}.''') 
-# sciencescore = input("enter score ")
-# print(sciencescore -2)
+# firstName = input("What is your first name ")
+# lastName = input("What is your last name ")
+# math = int(input(f'Dear {firstName} {lastName} enter your score in maths exam '))
+# science = int(input(f'Dear {firstName} {lastName} enter your score in science exam '))
+# english = int(input(f'Dear {firstName} {lastName} enter your score in english exam '))
+# add = math + science + english
+# mean = add/3
+# print(f'''Dear {firstName.capitalize()} {lastName.capitalize()}
+# Congratulation you have been offered admission into our University 
+# With an average score of {mean}.''') 
+sciencescore = 50
+print(sciencescore+"$")
 
