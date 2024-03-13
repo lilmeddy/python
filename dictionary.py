e192bbcb749b416fe5290aef77bf438053a1108f
lilmeddy <aghedopriscilla@gmail.com>
2024-03-13 09:23:59 +0100

    condition

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# create a dictionary for a car
+car={
+    "brand":"corolla",
+    "tyres":8,
+    "color":"carton color",
+    "gear":"automatic",
+    "carSeat":"5"
+}
+# print(car)
+car["brand"]="tesla"
+print(car)
