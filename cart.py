ee0fd9bd2d6247a9d919cb2340572929ff79f2e7
lilmeddy <aghedopriscilla@gmail.com>
2024-03-19 09:21:25 +0100

    for loop

--- cart.py
+++ cart.py
@@ -12,125 +12,142 @@
 user = input("Hello Dear customer what is your name")
 cart ={}
 
-product1,quantity1 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-cart[product1] = quantity1
-
-product2,quantity2 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-if product2 in list(cart.keys()) :
-    print("Product already exist ")
-    question= input("Do you want to change prodouct ")
-    if question == "yes" :
-       productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-       if productRep in list(cart.keys()) :
-           print("are you okay why are you repeating products idiot")
-       else :
+for prod in range(5):
+    product,quantity = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+    if product in list(cart.keys()) :
+       print("Product already exist ")
+       question= input("Do you want to change prodouct ")
+       if question == "yes" :
+          productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+          if productRep in list(cart.keys()) :
+            print("are you okay why are you repeating products idiot")
+          else :
            cart[productRep] = quantityRep 
-    else: 
-       print("Continue your shopping")
-
-else:
-    cart[product2] = quantity2
-
-product3,quantity3 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-if product3 in list(cart.keys()) :
-    print("Product already exist ")
-    question= input("Do you want to change prodouct ")
-    if question == "yes" :
-       productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-       if productRep in list(cart.keys()) :
-           print("are you okay why are you repeating products idiot")
-       else :
-           cart[productRep] = quantityRep 
-    else: 
-       print("Continue your shopping")
-       
-
-else:
-    cart[product3] = quantity3
-
-product4,quantity4 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-if product4 in list(cart.keys()) :
-    print("Product already exist ")
-    question= input("Do you want to change prodouct ")
-    if question == "yes" :
-       productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-       if productRep in list(cart.keys()) :
-           print("are you okay why are you repeating products idiot")
-       else :
-           cart[productRep] = quantityRep 
-    else: 
-       print("Continue your shopping")
-
-else:
-    cart[product4] = quantity4
-
-product5,quantity5= input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-if product5 in list(cart.keys()) :
-    print("Product already exist ")
-    question= input("Do you want to change prodouct ")
-    if question == "yes" :
-       productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-       if productRep in list(cart.keys()) :
-           print("are you okay why are you repeating products idiot")
-       else :
-           cart[productRep] = quantityRep 
-    else: 
-       print("Continue your shopping")
-
-else:
-    cart[product5] = quantity5
-
-print(f'''
-Your cart is 
-Product Item
-{list(cart.keys())[0]} {list(cart.values())[0]}
-{list(cart.keys())[1]} {list(cart.values())[1]}
-{list(cart.keys())[2]} {list(cart.values())[2]}
-{list(cart.keys())[3]} {list(cart.values())[3]}
-{list(cart.keys())[4]} {list(cart.values())[4]}
-      ''')
-
-print(f'''
- Dear user do you want to checkout or replace or clear cart
-To checkout enter 1
-To replace enter 2
-To clear enter 3
-To remove enter 4
-''')
-
-editing = int(input("What would you like to do "))
-total =list(cart.values())[0] + list(cart.values())[1] + list(cart.values())[2] + list(cart.values())[3] + list(cart.values())[4]
-if editing in [1,2,3,4]:
-    if editing == 1:
-      print(f'''
-        Dear {user} thanks for shopping with us 
-Your cart is 
-PRODUCT ITEM
-{list(cart.keys())[0]} {list(cart.values())[0]}
-{list(cart.keys())[1]} {list(cart.values())[1]}
-{list(cart.keys())[2]} {list(cart.values())[2]}
-{list(cart.keys())[3]} {list(cart.values())[3]}
-{list(cart.keys())[4]} {list(cart.values())[4]}
-Your total qualtity is {total}.
-      ''')
-
-    elif editing == 2:
-         newProduct, newItem = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
-         cart[newProduct] = newItem
-         del[editing]
-
-    elif editing == 3:
-          cart = {}
+       else: 
+         print("Continue your shopping")
+ 
+    else:
+       cart[product] = quantity
    
 
-    elif editing == 4 :
-        newEdit = input("what product would you like to adjust ")
-        cart[newEdit] = 0
+# product1,quantity1 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+# cart[product1] = quantity1
+# product2,quantity2 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+# if product2 in list(cart.keys()) :
+#     print("Product already exist ")
+#     question= input("Do you want to change prodouct ")
+#     if question == "yes" :
+#        productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+#        if productRep in list(cart.keys()) :
+#            print("are you okay why are you repeating products idiot")
+#        else :
+#            cart[productRep] = quantityRep 
+#     else: 
+#        print("Continue your shopping")
 
-    else:
-       print("invalid input")
-else: 
-    print("Invalid number pick 1-4")
+# else:
+#     cart[product2] = quantity2
+
+# product3,quantity3 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+# if product3 in list(cart.keys()) :
+#     print("Product already exist ")
+#     question= input("Do you want to change prodouct ")
+#     if question == "yes" :
+#        productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+#        if productRep in list(cart.keys()) :
+#            print("are you okay why are you repeating products idiot")
+#        else :
+#            cart[productRep] = quantityRep 
+#     else: 
+#        print("Continue your shopping")
+       
+
+# else:
+#     cart[product3] = quantity3
+
+# product4,quantity4 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+# if product4 in list(cart.keys()) :
+#     print("Product already exist ")
+#     question= input("Do you want to change prodouct ")
+#     if question == "yes" :
+#        productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+#        if productRep in list(cart.keys()) :
+#            print("are you okay why are you repeating products idiot")
+#        else :
+#            cart[productRep] = quantityRep 
+#     else: 
+#        print("Continue your shopping")
+
+# else:
+#     cart[product4] = quantity4
+
+# product5,quantity5= input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+# if product5 in list(cart.keys()) :
+#     print("Product already exist ")
+#     question= input("Do you want to change prodouct ")
+#     if question == "yes" :
+#        productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+#        if productRep in list(cart.keys()) :
+#            print("are you okay why are you repeating products idiot")
+#        else :
+#            cart[productRep] = quantityRep 
+#     else: 
+#        print("Continue your shopping")
+
+# else:
+#     cart[product5] = quantity5
+
+# print(f'''
+# Your cart is 
+# Product Item
+# {list(cart.keys())[0]} {list(cart.values())[0]}
+# {list(cart.keys())[1]} {list(cart.values())[1]}
+# {list(cart.keys())[2]} {list(cart.values())[2]}
+# {list(cart.keys())[3]} {list(cart.values())[3]}
+# {list(cart.keys())[4]} {list(cart.values())[4]}
+#       ''')
+
+# print(f'''
+#  Dear user do you want to checkout or replace or clear cart
+# To checkout enter 1
+# To replace enter 2
+# To clear enter 3
+# To remove enter 4
+# ''')
+
+# editing = int(input("What would you like to do "))
+# total =list(cart.values())[0] + list(cart.values())[1] + list(cart.values())[2] + list(cart.values())[3] + list(cart.values())[4]
+# if editing in [1,2,3,4]:
+#     if editing == 1:
+#       print(f'''
+#         Dear {user} thanks for shopping with us 
+# Your cart is 
+# PRODUCT ITEM
+# {list(cart.keys())[0]} {list(cart.values())[0]}
+# {list(cart.keys())[1]} {list(cart.values())[1]}
+# {list(cart.keys())[2]} {list(cart.values())[2]}
+# {list(cart.keys())[3]} {list(cart.values())[3]}
+# {list(cart.keys())[4]} {list(cart.values())[4]}
+# Your total qualtity is {total}.
+#       ''')
+
+#     elif editing == 2:
+#          newProduct, newItem = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+#          cart[newProduct] = newItem
+#          del[editing]
+
+#     elif editing == 3:
+#           cart = {}
+   
+
+#     elif editing == 4 :
+#         newEdit = input("what product would you like to adjust ")
+#         cart[newEdit] = 0
+
+#     else:
+#        print("invalid input")
+# else: 
+#     print("Invalid number pick 1-4")
 
 
 
--- loop.py
+++ loop.py
@@ -1,3 +1,17 @@
 # range function 
 # passing 1 parameter i.e (starts from zero ends at the parameter specified without returning the parameter)
-for 
+for num in range(10):
+    print(num)
+
+# passing 2 parameter i.e (starts fron zero and ends the parameter specified without returning the parameter )
+for num in range (4,10):
+    print(num)
+
+# passing 3 parameter i.e (starts from first paramter end at second without returning the second at a step of the third-1 e.g 2 step will be 1 )
+for num in range (4,10,2):
+    print(num) 
+
+numbers =[]
+for num in range(3):
+    numbers.append(input("Enter a number: "))
+print(numbers)
