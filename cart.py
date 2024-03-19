1d4b9868ea00d5cec40c40bef388e188870d78ba
lilmeddy <aghedopriscilla@gmail.com>
2024-03-19 09:35:32 +0100

    for loop

--- cart.py
+++ cart.py
@@ -16,9 +16,9 @@ for prod in range(5):
     product,quantity = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
     if product in list(cart.keys()) :
        print("Product already exist ")
-       question= input("Do you want to change prodouct ")
+       question= input("Do you want to change prodouct yes or no ")
        if question == "yes" :
-          productRep, quantityRep = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+          productRep, quantityRep = input("Enter new product: "),int(input("Enter the quantity of what you want to buy: "))
           if productRep in list(cart.keys()) :
             print("are you okay why are you repeating products idiot")
           else :
@@ -29,6 +29,27 @@ for prod in range(5):
     else:
        cart[product] = quantity
    
+print(f"{user.capitalize()} your cart is\nProduct Item\n{list(cart.keys())[0]} {list(cart.values())[0]}\n{list(cart.keys())[1]} {list(cart.values())[1]}\n{list(cart.keys())[2]} {list(cart.values())[2]}\n{list(cart.keys())[3]} {list(cart.values())[3]}\n{list(cart.keys())[4]} {list(cart.values())[4]}")
+editing = int(input("What would you like to do "))
+total =list(cart.values())[0] + list(cart.values())[1] + list(cart.values())[2] + list(cart.values())[3] + list(cart.values())[4]
+if editing in [1,2,3,4]:
+    if editing == 1:
+      print(f"{user.capitalize()} your cart is\nProduct Item\n{list(cart.keys())[0]} {list(cart.values())[0]}\n{list(cart.keys())[1]} {list(cart.values())[1]}\n{list(cart.keys())[2]} {list(cart.values())[2]}\n{list(cart.keys())[3]} {list(cart.values())[3]}\n{list(cart.keys())[4]} {list(cart.values())[4]}\n and your total is {total}")
+    elif editing == 2:
+         newProduct, newItem = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
+         cart[newProduct] = newItem
+         del[editing]
+    elif editing == 3:
+          cart = {}
+    elif editing == 4 :
+        newEdit = input("what product would you like to adjust ")
+        cart[newEdit] = 0
+    else:
+       print("invalid input")
+else: 
+    print("Invalid number pick 1-4")
+
+
 
 # product1,quantity1 = input("Enter what you want to buy: "),int(input("Enter the quantity of what you want to buy: "))
 # cart[product1] = quantity1
