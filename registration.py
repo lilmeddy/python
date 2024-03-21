7a86782a758e80545db388c4caa4497475f68a74
lilmeddy <aghedopriscilla@gmail.com>
2024-03-21 10:24:09 +0100

    editing

--- registration.py
+++ registration.py
@@ -1,12 +1,18 @@
 
 user =[]
 
-# for users in range(2):
-firstName,lastName,password = input("Enter your first name\n"),input("Enter your last name\n"),input("Enter your password\n")
-while len(password) < 10 :
-      print("Password must be greater than 10 characters")
-      password = input("Enter your password\n")
-user.append([firstName,lastName,password])
+for users in range(5):   
+    userName =input("Enter your username\n")
+    for i in user:
+       while i[0] == userName:
+            print("UserName taken")
+            userName= input("Enter your user name\n")
+    lastName,password = input("Enter your last name\n"),input("Enter your password\n")
+    while len(password) < 10 :
+        print("Password must be greater than 10 characters")
+        password = input("Enter your password\n")
+
+    user.append([userName,lastName,password])
 print(user)
 
 # login
