fc11c9ac2b032da0ee68656246a634571d8ba283
lilmeddy <aghedopriscilla@gmail.com>
2024-03-23 11:46:51 +0100

    gfghds

--- registration.py
+++ registration.py
@@ -1,7 +1,7 @@
 
 user =[]
 
-for users in range(5):   
+for users in range(1):   
     userName =input("Enter your username\n")
     for i in user:
        while i[0] == userName:
@@ -13,7 +13,32 @@ for users in range(5):
         password = input("Enter your password\n")
 
     user.append([userName,lastName,password])
+# login 
+    print(user)
+    for i in user:
+       print("Let's login and do some hehehe")
+       username =input("Enter your user name\n")
+    #    while i[0] != userName:
+    #         print("Invalid username")
+    #         userName= input("Enter your user name\n")
+       passWord= input("Enter your password\n")
+       print(user)
+       if i[0] == username:
+           print(i)
+       while passWord != i[2]:
+            #  print("Login Successful")
+           change = input("forgot password?? Enter yes to change password ")
+           if change == "yes":
+             i.pop(2)
+             print(user) 
+           newPassword = input("enter new password")
+           i.insert(2, newPassword)
+        # print(user)
 print(user)
 
-# login
+
+
+# print(user)
+
+
 
